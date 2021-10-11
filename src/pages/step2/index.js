d63bdc65f3a47bf9f0baea6cb987a20081834060
Alexander Ilyin <alexander.ilyin3@gmail.com>
2021-10-11 14:37:31 +0300

    step 2 layout finished

--- src/pages/step2/index.js
+++ src/pages/step2/index.js
@@ -1,7 +1,45 @@
+import { Typography as T } from '@material-ui/core'
+import { FlexBoxRow } from 'components/atoms/FlexBoxRow'
+import { SectionBox } from 'components/atoms/SectionBox'
+import { CheckBoxLabelBox } from 'components/molecules/CheckBoxLabelBox'
+import { InputBox } from 'components/molecules/InputBox'
+import { InputNumberBox } from 'components/molecules/InputNumberBox'
+import { SiteHeader } from 'components/molecules/SiteHeader.js'
+import { StepperComponent } from 'components/molecules/Stepper'
+import { FormControlButtons } from 'components/molecules/FormContolButtons'
+import { SectionWrapper } from 'components/atoms/SectionWrapper'
+import { OrderSummaryContainer } from 'components/atoms/OrderSummaryContainer'
+import { PageContentWrapper } from 'components/atoms/PageContentWrapper'
+import { OrderSummaryPlug } from 'components/atoms/OrderSummaryPlug'
+import { SiteFooter } from 'components/molecules/SiteFooter'
+import { CarCardsSection } from 'components/molecules/CarCardsSection'
+
+
 const Step2 = () => {
-  console.log('rendered')
+  
   return (
-    <div>STEP 2</div>
+    <>
+      <SiteHeader />
+      <StepperComponent />
+
+      <PageContentWrapper>
+        <SectionWrapper>
+          <SectionBox addSx={{
+            // paddingRight: '7%',
+            // paddingLeft: '7%'
+          }}>
+            <T variant='h1'> Vehicle Selection </T>
+            <InputNumberBox r labelText="Number of passengers"></InputNumberBox>
+            <CarCardsSection />
+          </SectionBox>
+          <FormControlButtons />
+        </SectionWrapper>
+        <OrderSummaryContainer >
+          <OrderSummaryPlug />
+        </OrderSummaryContainer>
+      </PageContentWrapper>
+      <SiteFooter />
+    </>
   )
 }
 
