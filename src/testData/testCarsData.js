0a88ad5c0c16abede9ea4e648fb98d3ee7e3076b
Alexander Ilyin <alexander.ilyin3@gmail.com>
2021-10-11 14:36:30 +0300

    added test cars data

---
+++ src/testData/testCarsData.js
@@ -0,0 +1,38 @@
+export const testCarData = [
+  {
+    carName: 'Shuttle - Local',
+    price: 25,
+    numberOfSeats: 8, 
+    picturePath: 'images/cars/Shuttle - Local.png',
+  },
+  {
+    carName: 'Nissan Pathfinder',
+    price: 25,
+    numberOfSeats: 1, 
+    picturePath: 'images/cars/Nissan Pathfinder.png',
+  },
+  {
+    carName: 'Ford Expedition Max',
+    price: 108,
+    numberOfSeats: 4, 
+    picturePath: 'images/cars/Ford Expedition Max.png',
+  },
+  {
+    carName: 'Chevy Suburban',
+    price: 25,
+    numberOfSeats: 4, 
+    picturePath: 'images/cars/Chevy Suburban.png',
+  },
+  {
+    carName: 'Ford Van',
+    price: 25,
+    numberOfSeats: 9, 
+    picturePath: 'images/cars/Ford Van.png',
+  },
+  {
+    carName: 'Motor Coach',
+    price: 25,
+    numberOfSeats: 42, 
+    picturePath: 'images/cars/Motor Coach.png',
+  },
+]
