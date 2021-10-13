265d90a33e9b4387737530fe2a47089c42789078
Alexander Ilyin <alexander.ilyin3@gmail.com>
2021-10-13 16:13:52 +0300

    updated test data

--- src/testData/testDestinationData.js
+++ src/testData/testDestinationData.js
@@ -2,10 +2,10 @@ export const testPickupData = [
   {
     group: 'test Airports',
     data: [
-      { name: 'Airport one'},
-      { name: 'Airport two'},
-      { name: 'Airport three'},
-      { name: 'Airport four'},
+      { name: 'Airport one' },
+      { name: 'Airport two' },
+      { name: 'Airport three' },
+      { name: 'Airport four' },
     ],
   },
   {
@@ -35,5 +35,91 @@ export const testDestinationData = [
       { name: 'Repulsor' },
       { name: 'Ave plaza' },
     ],
+  },
+]
+
+export const testPickupData2 = [
+  {
+    name: 'Airport one',
+    group: 'test Airports',
+    isAirport: true,
+  },
+  {
+    name: 'Airport two',
+    group: 'test Airports',
+    isAirport: true,
+  },
+  {
+    name: 'Airport three',
+    group: 'test Airports',
+    isAirport: true,
+  },
+  {
+    name: 'Airport four',
+    group: 'test Airports',
+    isAirport: true,
+  },
+  {
+    name: 'SJD one',
+    group: 'test Another group',
+    isAirport: true,
+  },
+  {
+    name: 'SJD two',
+    group: 'test Another group',
+    isAirport: true,
+  },
+  {
+    name: 'SJD three',
+    group: 'test Another group',
+    isAirport: true,
+  },
+  {
+    name: 'SJD four',
+    group: 'test Another group',
+    isAirport: true,
+  },
+  {
+    name: 'pickup one',
+    group: 'not Airport',
+    isAirport: false,
+  },
+  {
+    name: 'pickup two',
+    group: 'not Airport',
+    isAirport: false,
+  },
+  {
+    name: 'pickup three',
+    group: 'not Airport',
+    isAirport: false,
+  },
+]
+
+export const testDestinationData2 = [
+  {
+    name: 'Bonsai plaza',
+    group: 'test Condos',
+  },
+  {
+    name: 'Repulsor',
+    group: 'test Condos',
+    isAirport: true,
+  },
+  {
+    name: 'Ave plaza',
+    group: 'test Condos',
+  },
+  {
+    name: 'Bonsai plaza',
+    group: 'test Another group',
+  },
+  {
+    name: 'Repulsor',
+    group: 'test Another group',
+  },
+  {
+    name: 'Ave plaza',
+    group: 'test Another group',
   },
 ]
