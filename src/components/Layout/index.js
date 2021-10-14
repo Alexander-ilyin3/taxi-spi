5649617ae6644934a48e7ba8576857f769c1ba0c
Alexander Ilyin <alexander.ilyin3@gmail.com>
2021-10-14 15:17:56 +0300

    implementing redux

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -8,42 +8,48 @@ import {
 } from "react-router-dom"
 import { CssBaseline, ThemeProvider, Typography as T } from '@mui/material'
 import { FormProvider, useForm } from 'react-hook-form'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import Step1 from 'pages/step1'
 import Step2 from 'pages/step2'
 import { theme } from 'mui/theme.js'
+import { rootReducer } from 'redux/rootReducer'
 
 /* TEST COMPONENT //TODO */
 import { ConsoleFormStateButton } from 'testData/ConsoleFormStateButton'
 /* TEST COMPONENT //TODO */
 
 const Layout = () => {
-
+  const store = createStore(rootReducer)
   const methods = useForm()
+
   return (
-    <FormProvider {...methods} >
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Router>
-          <div>
-            <Switch>
-              <Route exact path='/'>
-                <Redirect to='/step-1' />
-              </Route>
-              <Route path="/step-1">
-                <Step1 />
-              </Route>
-              <Route path="/step-2">
-                <Step2 />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-      </ThemeProvider>
-      {/* TEST COMPONENT //TODO */}
-      <ConsoleFormStateButton />
-      {/* TEST COMPONENT //TODO */}
-    </FormProvider>
+    <Provider store={store}>
+      <FormProvider {...methods} >
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Router>
+            <div>
+              <Switch>
+                <Route exact path='/'>
+                  <Redirect to='/step-1' />
+                </Route>
+                <Route path="/step-1">
+                  <Step1 />
+                </Route>
+                <Route path="/step-2">
+                  <Step2 />
+                </Route>
+              </Switch>
+            </div>
+          </Router>
+        </ThemeProvider>
+        {/* TEST COMPONENT //TODO */}
+        <ConsoleFormStateButton />
+        {/* TEST COMPONENT //TODO */}
+      </FormProvider>
+    </Provider>
   )
 }
 
