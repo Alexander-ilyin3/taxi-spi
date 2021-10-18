1b9ffb3b82ccb01238a15e64707e05972728ae8d
Alexander Ilyin <alexander.ilyin3@gmail.com>
2021-10-18 13:22:23 +0300

    selected cards control and style

--- src/components/molecules/CarCardsSection.js
+++ src/components/molecules/CarCardsSection.js
@@ -1,23 +1,36 @@
 import { Box } from "@mui/material"
 import { CarCard } from "components/molecules/CarCard.js"
+import { Controller, useFormContext } from "react-hook-form"
 import { testCarData } from 'testData/testCarsData'
 
-export const CarCardsSection = () => {
+export const CarCardsSection = ({ setSelectedCar }) => {
+
+  const { control } = useFormContext()
 
   const cardsData = testCarData
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        // gap: 2,
-        justifyContent: 'space-between'
-      }}
-    >
-      {cardsData.map((data, i) => {
-        return <CarCard key={i} cardData={data}/>
-      })}
-    </Box>
+    <Controller
+      control={control}
+      name="selectedCar"
+      defaultValue={null}
+      render={({
+        field: { onChange, value }
+      }) => (
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            // gap: 2,
+            justifyContent: 'space-between'
+          }}
+        >
+          {cardsData.map((data, i) => {
+            return <CarCard key={i} cardData={{...data, index: i}} setSelectedCar={onChange} activeCar={value} />
+          })}
+        </Box>
+      )}
+    />
+
   )
 }
