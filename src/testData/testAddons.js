bc4f6e3783d0d2e279ea0efe707b776cebe0b489
Alexander Ilyin <alexander.ilyin3@gmail.com>
2021-10-20 14:16:50 +0300

    test addons data

---
+++ src/testData/testAddons.js
@@ -0,0 +1,52 @@
+export const testAddons = [
+  {
+    name: 'Oversized Golf Case',
+    src: 'images/addons/Golf Case.png',
+    price: 95,
+    description: 'Test description from the backend admin. This talks about the product and can be limited Test description from the backend admin. This talks about the product and can be limitedTest description from the backend admin. This talks about the product and can be limited '
+  },
+  {
+    name: '24 Corona Beer',
+    src: 'images/addons/Beer.png',
+    price: 35,
+    description: 'Cold on ice, a 6 pack of Coronas (x4)',
+    onlyForPrivatTransfers: true
+  },
+  {
+    name: 'Veuve Clicquot Champagne Bottle',
+    src: 'images/addons/Champagne.png',
+    price: 35,
+    description: 'Test description from the backend admin. This talks about the product and can be limited'
+  },
+  {
+    name: '12 Corona Beer',
+    src: 'images/addons/Beer.png',
+    price: 20,
+    description: 'Cold on ice, a 6 pack of Coronas (x4)',
+    onlyForPrivatTransfers: true
+  },
+  {
+    name: 'Arduini Sparkling Wine',
+    src: 'images/addons/Sparkling Wine.png',
+    price: 25,
+    description: 'Test description from the backend admin. This talks about the product and can be limited'
+  },
+  {
+    name: '30 min. Shopping Stop',
+    src: 'images/addons/Shopping Stop.png',
+    price: 35,
+    description: 'Test description from the backend admin. This talks about the product and can be limited'
+  },
+  {
+    name: '1 Hr. Shopping Stop',
+    src: 'images/addons/Shopping Stop.png',
+    price: 55,
+    description: 'Test description from the backend admin. This talks about the product and can be limited'
+  },
+  {
+    name: 'Toddler or Baby Seat',
+    src: 'images/addons/Baby Seat.png',
+    price: 20,
+    description: 'Test description from the backend admin. This talks about the product and can be limited'
+  },
+]
