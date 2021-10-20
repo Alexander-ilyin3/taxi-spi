392a3ba9ba878a311da2225b1e5a36061555dba9
Alexander Ilyin <alexander.ilyin3@gmail.com>
2021-10-20 17:49:53 +0300

    added molecule for step6

---
+++ src/components/molecules/CouponSection.js
@@ -0,0 +1,49 @@
+import { Typography as T } from '@mui/material'
+import { Box, useTheme } from '@mui/system'
+import { Button } from 'components/atoms/Button'
+import { Label } from 'components/atoms/InputLabel'
+import { InputBox } from 'components/molecules/InputBox'
+
+export const CouponSection = () => {
+  const { palette: { primary: { blue, white } } } = useTheme()
+  
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        maxWidth: '60%',
+        alignItems: 'center',
+        gap: 2,
+      }}
+    >
+      <Label
+        sx={{ margin: 0 }}
+      >
+        <T
+          variant="h5md"
+          sx={{
+            display: 'block',
+            width: 'max-content',
+
+          }}
+        >Coupon Code</T>
+      </Label>
+      <InputBox
+        name="couponCode"
+      >
+      </InputBox>
+      <Button
+        sx={{
+          paddingLeft: 6,
+          paddingRight: 6,
+          height: '100%',
+          backgroundColor: blue,
+          color: white,
+          fontWeight: 600,
+          borderRadius: '10px',
+        }}
+      ><T variant="h3" sx={{}} ></T> Apply</Button>
+
+    </Box>
+  )
+}
