4ed47a6e4ca0de20facbb5d33e081cf3bd4b489b
Alexander Ilyin <alexander.ilyin3@gmail.com>
2021-11-03 13:29:22 +0200

    middleware added to axios instance

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,4 +1,5 @@
 import axiosLib from 'axios'
+import { clearRequestsQueue, popRequestsQueue, pushRequestsQueue, setAxiosError } from 'redux/actions'
 const axios = axiosLib.default
 
 console.log('process env', process.env)
@@ -12,3 +13,30 @@ export const instance = axios.create({
     password: 'quughaedu6eex0Qu'
   }
 })
+
+let applied = false
+
+export const apiMiddleware = ({ dispatch }) => (next) => (action) => {
+  if (applied) return next(action) 
+  applied = true
+  
+  instance.interceptors.request.use((config) => {
+    dispatch(pushRequestsQueue())
+    // dispatch(setAxiosError(null))
+    return config
+  })
+
+  instance.interceptors.response.use((response) => {
+    dispatch(popRequestsQueue())
+    return response
+  }, (error) => {
+    console.log('error server ---- ', error.message)
+    // Do something with request error
+    dispatch(clearRequestsQueue())
+    dispatch(setAxiosError(error.message))
+    // throw new Error('aaaaaaaaaaa '+ error.message)
+    return
+  })
+  
+  next(action)
+}
