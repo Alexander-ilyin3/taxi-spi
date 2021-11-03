d9e725f258fe54ba1d8bdb2f36eb734eb700dcc1
Alexander Ilyin <alexander.ilyin3@gmail.com>
2021-11-03 13:25:47 +0200

    map state to parameters test button

--- src/testData/ConsoleFormStateButton.js
+++ src/testData/ConsoleFormStateButton.js
@@ -1,3 +1,4 @@
+import { mapStateToParams } from "helpers/mapStateForUpdateCart"
 const { useFormContext } = require("react-hook-form")
 
 
@@ -12,10 +13,16 @@ export const ConsoleFormStateButton = () => {
     console.log('Form errors', errors )
   }
 
+  const mapStateToPar = () => {
+    const result = mapStateToParams(getValues())
+    console.log('mapped state to params - ', result)
+  }
+
   return (
     <>
       <button style={{ position: 'sticky', bottom: 20, left: 20, backgroundColor: 'green', borderRadius: '20px', color: 'white' }} onClick={consoleFormState}>Form state</button>
       <button style={{ position: 'sticky', bottom: 20, left: 110, backgroundColor: 'green', borderRadius: '20px', color: 'white' }} onClick={consoleErrorState}>Form errors</button>
+      <button style={{ position: 'sticky', bottom: 20, left: 210, backgroundColor: 'green', borderRadius: '20px', color: 'white' }} onClick={mapStateToPar}>Map state to params</button>
     </>
   )
 }
