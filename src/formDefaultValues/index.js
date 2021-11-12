5d0c02d8735d4c02de6419ac5f4fa0fa214ff54a
Alexander Ilyin <alexander.ilyin3@gmail.com>
2021-11-12 21:11:41 +0200

    form default values added

---
+++ src/formDefaultValues/index.js
@@ -0,0 +1,76 @@
+export const defaultValues = {
+  1: {
+    pickupLocation: null,
+    destinationLocation: null,
+    isCustomDestination: false,
+    numberOfPassengers: '1',
+    roadTripReservation: false,
+  },
+  3: {
+    locationIsAirport: {
+      arrivalAirline: "",
+      bookingDate: null,
+      arrivalFlightNumber: "",
+      bookinglTime: null,
+      bookingDetailsNotes: "",
+    },
+    destinationIsAirport: {
+      bookingDetailsNotes: "",
+      departureAirline: "",
+      departureDate: null,
+      departureFlightNumber: "",
+      departureTime: null,
+    },
+    bothIsAirport: {
+      arrivalAirline: "",
+      arrivalDate: null,
+      arrivalFlightNumber: "",
+      arrivalTime: null,
+      bookingDetailsNotes: "",
+      departureAirline: "",
+      departureDate: null,
+      departureFlightNumber: "",
+      departureTime: null,
+    },
+    noneIsAirport: {
+      bookingDate: null,
+      bookingDetailsNotes: '',
+      bookinglTime: null,
+    }
+  },
+  5: {
+    additionalPhone: "",
+    address: "",
+    address2: "",
+    city: "",
+    confirmEmailAddress: "",
+    // country: null,
+    emailAddress: "",
+    firstName: "",
+    lastName: "",
+    mobilePhone: "",
+    postalCode: "",
+    // state: null,
+  }
+}
+
+// export const defaultValuesFor = (step) => {
+//   switch (step) {
+//     case 1:
+//       return {
+//         pickupLocation: null,
+//         destinationLocation: null,
+//         isCustomDestination: false,
+//         numberOfPassengers: '1',
+//         roadTripReservation: false,
+//       }
+
+//     case 4:
+//       return {
+
+//       }
+
+//     default:
+//       throw new Error('No case for defaultValues')
+//   }
+// }
