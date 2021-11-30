0628faff1d92ddd86242d691b0f33b23b1ef6639
Alexander Ilyin <alexander.ilyin3@gmail.com>
2021-11-30 15:27:57 +0200

    stepper updated with custon destination case

--- src/components/molecules/Stepper.js
+++ src/components/molecules/Stepper.js
@@ -1,23 +1,49 @@
 import { Typography as T, useTheme, Box, Stepper, Step, StepLabel } from "@mui/material"
 import { isEqual } from "underscore"
-import React, { useEffect, useMemo, useRef } from "react"
+import React, { useEffect, useMemo, useRef, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 
 import { StepIconComponent } from "components/atoms/StepIconComponent"
 import { StepperConnector } from "components/atoms/StepperConnector"
 import { setInitlalSteps, setStepsWithBooking, setStepsWithFlight } from "redux/actions"
-import { getIsAirportStates, getSteps } from "redux/selectors"
+import { getIsAirportStates, getIsCustomDestination, getSteps } from "redux/selectors"
 import useMediaQuery from '@mui/material/useMediaQuery'
+import { setCustomLocationStepsVariant } from "redux/actions"
+import { useHistory } from "react-router"
+import { useFormContext } from "react-hook-form"
 
-const StepperComponent = ({ activeStep }) => {
+const StepperComponent = () => {
   const theme = useTheme()
+  const { watch } = useFormContext()
   const { breakpoints: { down } } = theme
   const unMemosteps = useSelector(getSteps)
   const isAirportStates = useSelector(getIsAirportStates, isEqual)
+
+  // const reduxIsCustomDestination = useSelector(getIsCustomDestination)
+  // const formIsCustomDestination = watch('isCustomDestination')
+
+  // const isCustomDestination = formIsCustomDestination || reduxIsCustomDestination
+  const isCustomDestination = useSelector(getIsCustomDestination)
+  
   const steps = useMemo(() => unMemosteps)
   const dispatch = useDispatch()
   const stepperRef = useRef(null)
   const matches = useMediaQuery(down('sm'))
+
+  const history = useHistory()
+  const currentLocation = history.location.pathname
+
+  const [activeStep, setActiveStep] = useState(0)
+
+  useEffect(() => {
+    const regularStepsHistory = ['/step-1', '/step-2', '/step-3', '/step-4', '/step-5', '/step-6', '/step-7']
+    const airBnbStepsHistory = ['/step-1', '/step-3', '/step-5']
+
+    const usedArray = isCustomDestination ? airBnbStepsHistory : regularStepsHistory
+
+    setActiveStep(usedArray.indexOf(currentLocation))
+
+  }, [isCustomDestination, currentLocation])
 
   useEffect(() => {
     if (!steps.length) {
@@ -27,12 +53,15 @@ const StepperComponent = ({ activeStep }) => {
   }, [])
 
   useEffect(() => {
+
+    if (isCustomDestination) return dispatch(setCustomLocationStepsVariant())
+
     if (isAirportStates?.destinationIsAirport || isAirportStates?.locationIsAirport) {
       dispatch(setStepsWithFlight())
     } else {
       dispatch(setStepsWithBooking())
     }
-  }, [isAirportStates])
+  }, [isAirportStates, isCustomDestination])
 
   return (
     <Box sx={{
