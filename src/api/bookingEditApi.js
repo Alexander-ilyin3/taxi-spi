41ca48f0b2a7d532471bc28baea3a4d97346b8ff
Alexander Ilyin <alexander.ilyin3@gmail.com>
2021-12-03 22:09:51 +0200

    added skip

--- src/api/bookingEditApi.js
+++ src/api/bookingEditApi.js
@@ -3,11 +3,15 @@ import { instance } from 'api/instance'
 export const bookingEdit = {
   checkout: (data) => {
     // const userAgent = window.navigator.userAgent || "userAgent - undefined";
+    if (data?.order_id === null) {
+      return Promise.resolve('skip')
+    }
+
     return (
       instance
         .post('bookings-edit', { ...data })
         .then(res => res?.data)
-        // .catch(err => err )
+      // .catch(err => err )
     )
   },
 }
