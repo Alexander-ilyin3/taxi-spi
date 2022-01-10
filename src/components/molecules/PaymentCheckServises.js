1dc97cb2f0b34d3aa1d51818edfc2fabfd87cf34
Viktor Limishenko <dictor93@gmail.com>
2022-01-10 19:43:53 +0200

    Allow users to add event to the google calendar

--- src/components/molecules/PaymentCheckServises.js
+++ src/components/molecules/PaymentCheckServises.js
@@ -3,10 +3,29 @@ import { Box } from "@mui/system"
 import { calendars } from "api/calendarsApi"
 import { reduceIconPath } from "helpers/reduceIconPath"
 import { useSelector } from "react-redux"
-import { getBookingId } from "redux/selectors"
+import { getBookingId, getGlobalStepsData } from "redux/selectors"
+
+const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID
+const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"]
+const SCOPES = "https://www.googleapis.com/auth/calendar"
 
 export const PaymentCheckServises = ({ bookingId }) => {
+  const data = useSelector(getGlobalStepsData)
+  console.log(data)
+  const {
+    location,
+    booking_date,
+    booking_time,
+    destination,
+    vehicle,
+    total,
+    notes,
+  } = data
   const { palette: { primary: { blue } } } = useTheme()
+
+  const date = new Date(`${booking_date}:${booking_time}`)
+  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
 
   const icalOnClick = async () => {
 
@@ -18,10 +37,57 @@ export const PaymentCheckServises = ({ bookingId }) => {
     window.location.assign(`${domain}/wp-content/plugins/sjd-booking/libraries/icalendar/icalendar.php?order_id=${calenderResponse?.order_id}`) //TODO
   }
 
+  const googleCalendarHandler = () => {
+    window.gapi.load('client:auth2', () => {
+      console.log('loaded client')
+
+      window.gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      })
+
+
+      window.gapi.client.load('calendar', 'v3', () => console.log('bam!'))
+
+      window.gapi.auth2.getAuthInstance().signIn()
+      .then(() => {
+        console.log('creating event')
+        var event = {
+          'summary': `${location?.name} - ${destination?.name}`,
+          'location': location?.name,
+          'description': `${vehicle?.vehicle_count} ${vehicle?.name}.\nTotal: ${total}$.\n${notes}`,
+          'start': {
+            'dateTime': date,
+            'timeZone': timezone,
+          },
+          'end': {
+            'dateTime': date,
+            'timeZone': timezone,
+          },
+          'reminders': {
+            'useDefault': true,
+          }
+        }
+
+        var request = window.gapi.client.calendar.events.insert({
+          'calendarId': 'primary',
+          'resource': event,
+        })
+        console.log({ request })
+        request.execute(event => {
+          console.log(event)
+          window.open(event.htmlLink)
+        })
+      })
+    })
+  }
+
   return (
     <Button
       variant="buttonEffectsWrapper"
-      onClick={icalOnClick}
+      onClick={googleCalendarHandler}
       sx={{
         display: 'flex',
         justifyContent: 'center',
@@ -34,9 +100,9 @@ export const PaymentCheckServises = ({ bookingId }) => {
         height="50px"
         width="50px"
       >
-        <img src={reduceIconPath('images/ical.svg')}></img>
+        <img height="50" alt="google calendar" src={reduceIconPath('images/GoogleCalendar.png')}></img>
       </Box>
-      <T variant="h5sb" color={blue}>Add to ICal</T>
+      <T variant="h5sb" color={blue}>Add to Google Calendar</T>
     </Button>
   )
 }
