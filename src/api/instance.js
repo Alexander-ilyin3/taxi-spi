f02a4dfc8159f8ecdda4c65d05cebc7b24ecdec4
Viktor Limishenko <dictor93@gmail.com>
2022-05-10 15:56:23 +0300

    Use base url from the window variable

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,30 +1,44 @@
-import axiosLib from 'axios'
-import { clearRequestsQueue, popRequestsQueue, pushRequestsQueue, setAxiosError } from 'redux/actions'
-const axios = axiosLib.default
+import axiosLib from "axios";
+import {
+  clearRequestsQueue,
+  popRequestsQueue,
+  pushRequestsQueue,
+  setAxiosError,
+} from "redux/actions";
+const axios = axiosLib.default;
 
 export const instance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
-})
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? process.env.REACT_APP_BASE_URL
+      : window.sjd_inline_script.api_domain,
+});
 
-let applied = false
+let applied = false;
 
-export const apiMiddleware = ({ dispatch }) => (next) => (action) => {
-  if (applied) return next(action) 
-  applied = true
-  
-  instance.interceptors.request.use((config) => {
-    dispatch(pushRequestsQueue())
-    return config
-  })
+export const apiMiddleware =
+  ({ dispatch }) =>
+  (next) =>
+  (action) => {
+    if (applied) return next(action);
+    applied = true;
 
-  instance.interceptors.response.use((response) => {
-    dispatch(popRequestsQueue())
-    return response
-  }, (error) => {
-    dispatch(clearRequestsQueue())
-    dispatch(setAxiosError(error.message))
-    return
-  })
-  
-  next(action)
-}
+    instance.interceptors.request.use((config) => {
+      dispatch(pushRequestsQueue());
+      return config;
+    });
+
+    instance.interceptors.response.use(
+      (response) => {
+        dispatch(popRequestsQueue());
+        return response;
+      },
+      (error) => {
+        dispatch(clearRequestsQueue());
+        dispatch(setAxiosError(error.message));
+        return;
+      }
+    );
+
+    next(action);
+  };
