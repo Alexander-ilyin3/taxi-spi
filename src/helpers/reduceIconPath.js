ea2c27a1c5c525d5a657621744be0d3562e8b5d3
Viktor Limishenko <dictor93@gmail.com>
2022-05-10 18:26:59 +0300

    use images domain images url

--- src/helpers/reduceIconPath.js
+++ src/helpers/reduceIconPath.js
@@ -3,5 +3,7 @@ export const reduceIconPath = (path) => {
     return path
   }
 
-  return 'https://sjd-taxi.requestumdemo.com/wp-content/plugins/sjd-booking/app/build/' + path
+  const url = new URL(window.sjd_inline_script.api_domain || 'https://rebuild.sjdtaxi.com');
+  
+  return `${url.host}/wp-content/plugins/sjd-booking/app/build/${path.replace(/^\//, '')}`
 }
