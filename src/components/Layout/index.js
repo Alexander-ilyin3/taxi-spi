e300f3f65b8faba8640c17564b63c1e31b93b65f
Viktor Limishenko <dictor93@gmail.com>
2022-06-17 12:09:02 +0300

    Parse booking parameters from link

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,16 +3,8 @@ import {
   HashRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect,
 } from "react-router-dom";
-import { CssBaseline, ThemeProvider, Typography as T } from "@mui/material";
-import jss from "jss";
-import increaseSpecificity from "jss-increase-specificity";
-import { StylesProvider, jssPreset } from "@mui/styles";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 
 import Step1 from "pages/step1";
 import Step2 from "pages/step2";
@@ -22,8 +14,6 @@ import Step5 from "pages/step5";
 import Step6 from "pages/step6";
 import Step7 from "pages/step7";
 
-import { theme } from "mui/theme.js";
-import { store } from "redux/reducers/rootReducer";
 
 /* TEST COMPONENT //TODO */
 import { ConsoleFormStateButton } from "testData/ConsoleFormStateButton";
@@ -32,22 +22,22 @@ import { ConsoleFormStateButton } from "testData/ConsoleFormStateButton";
 import { ErrorPage } from "components/organisms/ErrorPage";
 import { ErrorRedirect } from "components/organisms/ErrorRedirect";
 import { PageLoading } from "components/organisms/PageLoading";
-import { apiMiddleware } from "api/instance";
-import { useFeeUpdate } from "helpers/customHooks";
+import { useApiCall, useFeeUpdate } from "helpers/customHooks";
 import { useFormContext } from "react-hook-form";
 import { useDependenceReduxStateController } from "components/organisms/DependenceReduxStateController";
-
-// const composed =
-// const composedEnhancer = applyMiddleware(apiMiddleware, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__)
-
-// const jss = create({
-//   plugins: [...jssPreset().plugins, increaseSpecificity({ repeat: 3 })],
-// });
-const newJss = jss.setup(jssPreset()).use(increaseSpecificity({ repeat: 3 }));
+import { session } from "api/sessionApi";
+import { setGlobalStepsData } from "redux/actions";
+import qs from "qs";
 
 const Layout = () => {
   const methods = useFormContext();
   useDependenceReduxStateController(methods);
+  const search = window.location.search.replace(/\/$/, '').replace(/^\?/, '');
+  useApiCall({
+    handler: session.updateSession,
+    action: setGlobalStepsData,
+    params: qs.parse(search),
+  });
   useFeeUpdate();
   return (
     <>
